7693ae9b0905aebd9da3db36cb9ba4b2cfbab6b3
freedy86 <freedy8601@gmail.com>
2018-09-18 18:43:40 -0300

    Update docker-entrypoint.sh

--- kibana/docker-entrypoint.sh
+++ kibana/docker-entrypoint.sh
@@ -7,6 +7,6 @@ if [[ "$1" == -* ]]; then
 	set -- kibana "$@"
 fi
 
-sed -ri "s!^(\#\s*)?(elasticsearch\.url:).*!\2 'http://${ELASTICSEARCH_SERVICE_HOST}:9200'!" /opt/kibana/config/kibana.yml
+sed -ri "s!^(\#\s*)?(elasticsearch\.url:).*!\2 'http://${ELASTICSEARCH_SERVICE_HOST}:9200'!" /etc/kibana/kibana.yml
 
 exec "$@"
